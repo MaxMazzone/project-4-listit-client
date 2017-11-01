75d228871ff329252262ad5bd2656d04e757992d
stgeorge3 <amaxmazzone@gmail.com>
2017-11-01 13:21:14 -0400

    Updates apartments model
    Updates apartments model and adds ownership by user.

--- app/models/apartment.js
+++ app/models/apartment.js
@@ -2,9 +2,10 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   address: DS.attr('string'),
-  rent: DS.attr('integer'),
-  bedrooms: DS.attr('integer'),
-  bathrooms: DS.attr('integer'),
+  rent: DS.attr('number'),
+  bedrooms: DS.attr('number'),
+  bathrooms: DS.attr('number'),
   description: DS.attr('string'),
-  petsAllowed: DS.attr('boolean')
+  petsAllowed: DS.attr('boolean'),
+  user: DS.belongsTo('user')
 });
