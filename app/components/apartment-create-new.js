0c3f57ead68a54c51eb379a66589b7c5341c9dde
stgeorge3 <amaxmazzone@gmail.com>
2017-11-01 15:04:54 -0400

    Adds create action for apartment Adds a create action in apartment-create-new which is passed up and saved in apartments/new

--- app/components/apartment-create-new.js
+++ app/components/apartment-create-new.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  newApartment: {
+    address: null,
+    rent: null,
+    bedrooms: null,
+    bathrooms: null,
+    description: null,
+    petsAllowed: false
+
+  },
+  actions: {
+    createApartment() {
+      this.sendAction('createApartment', this.get('newApartment'));
+      return this.set('newApartment', {});
+    }
+  }
 });
--- app/routes/apartments/new.js
+++ app/routes/apartments/new.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    createApartment (apartment) {
+    let newApartment = this.get('store').createRecord('apartment', apartment);
+      newApartment.save()
+      this.transitionTo('apartments')
+    }
+  }
 });
