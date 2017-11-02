6c9f0bdabf885f55f0822066bf38874d99f17bc2
stgeorge3 <amaxmazzone@gmail.com>
2017-11-02 11:27:16 -0400

    fix typo in deployment url

--- config/environment.js
+++ config/environment.js
@@ -52,7 +52,7 @@ module.exports = function (environment) {
   if (environment === 'production') {
     ENV.rootURL = '/project-4-listit-client';
     ENV.locationType = 'hash';
-    ENV.apiHost = 'https://possessed-moonlight-45582.herokuapp.com/';
+    ENV.apiHost = 'https://possessed-moonlight-45582.herokuapp.com';
   }
 
   return ENV;
