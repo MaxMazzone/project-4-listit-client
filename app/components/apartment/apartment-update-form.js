5049d77ca97947bac87189706ac3882eb68b7ae3
stgeorge3 <amaxmazzone@gmail.com>
2017-11-02 10:17:43 -0400

    Add edit listing form
    Adds form to edit listing

--- app/components/apartment/apartment-update-form.js
+++ app/components/apartment/apartment-update-form.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-});
+    actions: {
+    editList () {
+    console.log(this.get('apartment.address'))
+     this.sendAction('editListing', this.get('apartment'));
+    },
+  }
+  });
